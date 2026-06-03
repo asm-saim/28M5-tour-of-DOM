5d63f0e65fdc3a8b56285eafa054712f9c83ecab
asm-saim <saim777m@gmail.com>
2026-06-03 23:32:11 +0600

    dom-styles in internal/external/and in css files

--- js/hello.js
+++ js/hello.js
@@ -1,2 +1,17 @@
-// console.log("Hello from outside");
-// console.log(document);
+// const sections=document.getElementsByTagName('section');
+// for(let section of sections)
+// {
+//     console.log(section.innerText);
+// }
+
+const sections= document.querySelectorAll('section');
+for(let section of sections){
+    section.style.backgroundColor='lightblue';
+    section.style.border='2px solid blue';
+    section.style.margin='20px';
+    section.style.borderRadius='20px';
+} 
+
+//
+const values=document.getElementById('tour');
+values.style.backgroundColor='grey';
